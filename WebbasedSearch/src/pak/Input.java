98e2f5202638a455044e40a0ebf5eb239604c6c1
cmarshall19 <cmarshall19@uco.edu>
2022-02-21 00:20:18 -0600

    Shifter Added

--- WebbasedSearch/src/pak/Input.java
+++ WebbasedSearch/src/pak/Input.java
@@ -1,17 +1,23 @@
 package pak;
 
 import java.awt.Button;
-import java.awt.Frame;
-import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class Input {
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class Input implements ActionListener {
+	JFrame f = new JFrame();
+	Button b = new Button();
+	JTextArea t1 = new JTextArea();
+	JTextArea t2 = new JTextArea();
+	static ArrayList<ArrayList<String>> box = new ArrayList<ArrayList<String>>();
+
 	Input() {
+
 		// Component Declarations
-		Frame f = new Frame();
-		Button b = new Button();
-		TextField t1 = new TextField();
-		TextField t2 = new TextField();
 
 		// Add Components to Frame
 		f.add(b);
@@ -20,9 +26,10 @@ public class Input {
 
 		// Apply settings to components
 		t1.setBounds(20, 100, 400, 400);
-		t2.setBounds(420, 100, 400, 400);
-		b.setBounds(820, 470, 80, 30);
+		t2.setBounds(520, 100, 400, 400);
+		b.setBounds(820, 600, 80, 30);
 		b.setLabel("Engage!");
+		b.addActionListener(this);
 		f.setSize(1000, 1000);
 		f.setTitle("Webbased Search Engine");
 		f.setLayout(null);
@@ -30,28 +37,60 @@ public class Input {
 
 	}
 
+	static public void print() {
+		for (int i = 0; i < box.size(); i++) {
+			for (int j = 0; j < box.get(i).size(); j++)
+				System.out.print(box.get(i).get(j) + " ");
+			System.out.print("\n");
+		}
+	}
+
 	public static void main(String[] args) {
 
 		Input display = new Input();
+	}
 
-		ArrayList<String> l = new ArrayList<String>();
-
-		l.add("Pipe");
-		l.add("and");
-		l.add("Filter");
-
-		for (int i = 0; i < l.size(); i++) {
-			System.out.println(l.get(i) + " ");
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String[] rowWords = t1.getText().split("\n");
+		for (int i = 0; i < t1.getLineCount(); i++) {
+			String[] colWords = rowWords[i].split(" ");
+			ArrayList<String> t = new ArrayList<String>();
+			for (int j = 0; j < colWords.length; j++) {
+				t.add(colWords[j]);
+			}
+			box.add(t);
 		}
-		System.out.println("\n");
 
-		Organizer test = new Organizer();
-		test.Sort(l);
+		Shifter circ = new Shifter();
+		Organizer alpha = new Organizer();
+		/******************************************************************
+		 * Testing *
+		 ******************************************************************/
 
-		for (int i = 0; i < l.size(); i++) {
-			System.out.println(l.get(i) + " ");
+//Pipe and Filter
+//Software Architecture in Practice
+
+		// Test of both Shifter and Organizer
+//		ArrayList<String> test = alpha.Sort(circ.circShift(box));
+
+		// Isolated Shifting Test
+		ArrayList<String> test = circ.circShift(box);
+
+		// Isolated Organizer Test
+//		ArrayList<String> test = new ArrayList<String>();
+//		test.add("Filter and Pipe");
+//		test.add("Filter Pipe and");
+//		test.add("Pipe and Filter");
+//		test.add("Architecture in Practice Software");
+//		test.add("in Practice Software Architecture");
+//		test.add("Practice Software Architecture in");
+//		test.add("Software Architecture in Practice");
+//		alpha.Sort(test);
+
+		for (int i = 0; i < test.size(); i++) {
+			System.out.println(test.get(i));
 		}
 
 	}
-
 }
--- WebbasedSearch/src/pak/Shifter.java
+++ WebbasedSearch/src/pak/Shifter.java
@@ -1,5 +1,42 @@
 package pak;
 
+import java.util.ArrayList;
+
 public class Shifter {
+
+	public static ArrayList<String> circShift(ArrayList<ArrayList<String>> list) {
+//		for (int i = 0; i < wor.size(); i++) {
+//			String temp = wor.get(0);
+//			for (int j = 0; j < wor.size() - 1; j++) {
+//				wor.set(j, wor.get(j + 1));
+//			}
+//			wor.set(wor.size() - 1, temp);		
+//			for (int j = 0; j < wor.size(); j++) {
+//				System.out.print(wor.get(j) + " ");
+//			}
+//			System.out.println("\n");
+//		}
+
+		ArrayList<String> shiftedList = new ArrayList<String>();
+		for (int i = 0; i < list.size(); i++) {
+			ArrayList<String> t_list = list.get(i);
+
+			for (int j = 0; j < t_list.size(); j++) {
+				String temp = t_list.get(0);
+				for (int k = 0; k < t_list.size() - 1; k++) {
+					t_list.set(k, t_list.get(k + 1));
+				}
+				t_list.set(t_list.size() - 1, temp);
+				temp = "";
+				for (int k = 0; k < t_list.size(); k++) {
+					temp += t_list.get(k) + " ";
+				}
+				shiftedList.add(temp);
+			}
+			// System.out.println(shiftedList.get(i));
+		}
+
+		return shiftedList;
+	}
 
 }
