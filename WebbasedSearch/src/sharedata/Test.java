4007796cc45ab2984e504e5a1a42bb712a4fe0a0
Agent15 <Afroninja528491@gmail.com>
2022-03-29 22:22:55 -0500

    Update Test.java

--- WebbasedSearch/src/sharedata/Test.java
+++ WebbasedSearch/src/sharedata/Test.java
@@ -25,6 +25,117 @@ public class Test {
 		 */
 		LineStorage l = new LineStorage();
 		Input display = new Input(l);
+		
+		LineStorage l = new LineStorage();
+        //Line 1
+        l.addLine();
+        l.addWord();
+        l.addChar('H');
+        l.addChar('e');
+        l.addChar('l');
+        l.addChar('l');
+        l.addChar('o');
+        l.addWord();
+        l.addChar('W');
+        l.addChar('o');
+        l.addChar('r');
+        l.addChar('l');
+        l.addChar('d');
+        l.addChar('!');
+        
+        //Line 2
+        l.addLine();
+        l.addWord();
+        l.addChar('S');
+        l.addChar('h');
+        l.addChar('a');
+        l.addChar('r');
+        l.addChar('e');
+        l.addChar('d');
+        l.addWord();
+        l.addChar('D');
+        l.addChar('a');
+        l.addChar('t');
+        l.addChar('a');
+        l.addWord();
+        l.addChar('D');
+        l.addChar('e');
+        l.addChar('s');
+        l.addChar('i');
+        l.addChar('g');
+        l.addChar('n');
+        
+        //Line 3
+        l.addLine();
+        l.addWord();
+        l.addChar('A');
+        l.addChar('n');
+        l.addChar('o');
+        l.addChar('t');
+        l.addChar('h');
+        l.addChar('e');
+        l.addChar('r');
+        l.addWord();
+        l.addChar('L');
+        l.addChar('i');
+        l.addChar('n');
+        l.addChar('e');
+        int f=0, g=0, h=0;
+        //Display LineStorage
+        System.out.println("--LINESTORAGE DISPLAY--");
+        while(true)
+        {
+            if(l.getChar(f, g, h) == 0)
+            {
+                h = 0;
+                g++;
+                System.out.print(' ');
+            }
+            if(l.getChar(f, g, h) == 0)
+            {
+                g = 0;
+                f++;
+                System.out.print('\n');
+            }
+            if(l.getChar(f, g, h) == 0)
+            {
+                break;
+            }
+            System.out.print(l.getChar(f, g, h));
+            h++;
+            
+        }
+        System.out.println("--END LINESTORAGE DISPLAY--");
+        
+        //Test Alphabetizer
+        System.out.println("--ALPHABETIZER TEST--");
+        Alphabetizer a = new Alphabetizer(l);
+        //a.Sort();
+        f=0; g=0; h=0;
+        while(true)
+        {
+            if(a.getChar(f, g, h) == 0)
+            {
+                h = 0;
+                g++;
+                System.out.print(' ');
+            }
+            if(a.getChar(f, g, h) == 0)
+            {
+                g = 0;
+                f++;
+                System.out.print('\n');
+            }
+            if(a.getChar(f, g, h) == 0)
+            {
+                break;
+            }
+            System.out.print(a.getChar(f, g, h));
+            h++;
+            
+        }
+        System.out.println("--END ALPHABETIZER TEST--");
+    }
 
 	}
 }
