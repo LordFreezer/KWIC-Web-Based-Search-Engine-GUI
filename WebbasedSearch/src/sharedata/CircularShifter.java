9f930d922f1ef668307a38d4a484b090c8679ed7
Agent15 <Afroninja528491@gmail.com>
2022-03-29 20:46:59 -0500

    Uploaded a general Idea of CircularShifter

--- WebbasedSearch/src/sharedata/CircularShifter.java
+++ WebbasedSearch/src/sharedata/CircularShifter.java
@@ -6,21 +6,26 @@ import java.util.ArrayList;
  * 
  */
 public class CircularShifter {
+	int[] lineLengths;
+	private LineStorage reference;
 
 	/**
-	 * Default constructor
+	 * Constructor: Fills in the Reference attribute and stores is line lengths in lineLengths
 	 */
-	public CircularShifter() {
-		cdIndices = new ArrayList<>();
+	public CircularShifter(LineStorage ls) {
+		//Fill the reference attribute
+		reference = ls;
+		
+		//Store the length of each line
+		lineLengths = new int[reference.linecount()];
+		for (int g = 0; g < lineLengths.length; g++)
+		{
+			lineLengths[g] = wordCountAt(g);
+		}
 	}
-
+	
 	/**
-	 * 
-	 */
-	public ArrayList<Integer> cdIndices;
-
-	/**
-	 * @return
+	 * We may not even need this function :P
 	 */
 	public void makeShift() {
 		// TODO implement here
@@ -28,26 +33,26 @@ public class CircularShifter {
 	}
 
 	/**
-	 * @param int
-	 * @param int
-	 * @param int
-	 * @return
+	 * Returns the k'th character in the j'th word in the i'th line in the Circular shift array
 	 */
 	public char getChar(int i, int j, int k) {
-		// TODO implement here
-		return '0';
-	}
-
-	/**
-	 * @param int
-	 * @param int
-	 * @param int
-	 * @param char
-	 * @return
-	 */
-	public void setChar(int i, int j, int k, char c) {
-		// TODO implement here
-
+		//Find which line from LineStorage to pick from
+		int g;
+		for(g = 0; g < reference.linecount(); g++)
+		{
+			i -= lineLengths[g];
+			if (i < 0)
+			{
+				i += lineLengths[g];
+				break;
+			}
+		}
+		
+		int h = /*Find out which word this line should start on*/;
+		if(/*The word we need to pick is after this line would wrap around*/)
+			/*"Wrap around" h*/;
+		
+		return reference.getChar(g, h, k);
 	}
 
 }
