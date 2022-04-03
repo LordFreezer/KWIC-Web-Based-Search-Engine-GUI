ac83a402f3ff59e97329f3dc6eda5d8d8e949473
Agent15 <Afroninja528491@gmail.com>
2022-04-03 15:33:02 -0500

    Update Test.java

--- WebbasedSearch/src/sharedata/Test.java
+++ WebbasedSearch/src/sharedata/Test.java
@@ -23,119 +23,62 @@ public class Test {
 		 * l.getChar(f, g, h); System.out.print(c); } System.out.print(' '); }
 		 * System.out.print('\n'); }
 		 */
-		LineStorage l = new LineStorage();
 		Input display = new Input(l);
-		
-		LineStorage l = new LineStorage();
-        //Line 1
-        l.addLine();
-        l.addWord();
-        l.addChar('H');
-        l.addChar('e');
-        l.addChar('l');
-        l.addChar('l');
-        l.addChar('o');
-        l.addWord();
-        l.addChar('W');
-        l.addChar('o');
-        l.addChar('r');
-        l.addChar('l');
-        l.addChar('d');
-        l.addChar('!');
-        
-        //Line 2
-        l.addLine();
-        l.addWord();
-        l.addChar('S');
-        l.addChar('h');
-        l.addChar('a');
-        l.addChar('r');
-        l.addChar('e');
-        l.addChar('d');
-        l.addWord();
-        l.addChar('D');
-        l.addChar('a');
-        l.addChar('t');
-        l.addChar('a');
-        l.addWord();
-        l.addChar('D');
-        l.addChar('e');
-        l.addChar('s');
-        l.addChar('i');
-        l.addChar('g');
-        l.addChar('n');
-        
-        //Line 3
-        l.addLine();
-        l.addWord();
-        l.addChar('A');
-        l.addChar('n');
-        l.addChar('o');
-        l.addChar('t');
-        l.addChar('h');
-        l.addChar('e');
-        l.addChar('r');
-        l.addWord();
-        l.addChar('L');
-        l.addChar('i');
-        l.addChar('n');
-        l.addChar('e');
-        int f=0, g=0, h=0;
+        LineStorage l = new LineStorage();
+        addLine(l, "Hello World");
+        addLine(l, "This is a test");
+        addLine(l, "Shared Data Design");
+        addLine(l, "Another line");
+        addLine(l, "Big Beefy boi with lots and lots of words");
+
         //Display LineStorage
         System.out.println("--LINESTORAGE DISPLAY--");
-        while(true)
-        {
-            if(l.getChar(f, g, h) == 0)
-            {
-                h = 0;
-                g++;
-                System.out.print(' ');
-            }
-            if(l.getChar(f, g, h) == 0)
-            {
-                g = 0;
-                f++;
-                System.out.print('\n');
-            }
-            if(l.getChar(f, g, h) == 0)
-            {
-                break;
-            }
-            System.out.print(l.getChar(f, g, h));
-            h++;
-            
-        }
+        Display(l);
         System.out.println("--END LINESTORAGE DISPLAY--");
+        
+        //Test Circular Shifter
+        System.out.println("--CIRCULAR SHIFTER TEST--");
+        CircularShifter cs = new CircularShifter(l);
+        Display(cs);
+        System.out.println("--END CIRCULAR SHIFTER TEST--");
         
         //Test Alphabetizer
         System.out.println("--ALPHABETIZER TEST--");
-        Alphabetizer a = new Alphabetizer(l);
-        //a.Sort();
-        f=0; g=0; h=0;
-        while(true)
-        {
-            if(a.getChar(f, g, h) == 0)
-            {
+        Alphabetizer az = new Alphabetizer(cs);
+        Display(az);
+        System.out.println("--END ALPHABETIZER TEST--");
+    }
+
+    public static void Display(ILineSet ls) {
+        int f = 0, g = 0, h = 0;
+        while (true) {
+            if (ls.getChar(f, g, h) == 0) {
                 h = 0;
                 g++;
                 System.out.print(' ');
             }
-            if(a.getChar(f, g, h) == 0)
-            {
+            if (ls.getChar(f, g, h) == 0) {
                 g = 0;
                 f++;
                 System.out.print('\n');
             }
-            if(a.getChar(f, g, h) == 0)
-            {
+            if (ls.getChar(f, g, h) == 0) {
                 break;
             }
-            System.out.print(a.getChar(f, g, h));
+            System.out.print(ls.getChar(f, g, h));
             h++;
-            
         }
-        System.out.println("--END ALPHABETIZER TEST--");
     }
-
-	}
+    
+    public static void addLine(LineStorage ls, String st)
+    {
+        ls.addLine();
+        ls.addWord();
+        for(int g=0; g < st.length(); g++)
+        {
+            if(st.charAt(g) == ' ')
+                ls.addWord();
+            else ls.addChar(st.charAt(g));
+        }
+    }
 }
